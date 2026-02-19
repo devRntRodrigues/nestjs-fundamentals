30fd9a261d47804c86411758c6b8b91a8c362b97
devRnTRodrigues <devrenatorodrigues@gmail.com>
2026-02-19 08:56:40 -0300

    dd CoffeeRating module and service, integrate Database module, and enhance Coffees module with CoffeeBrands factory

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { DatabaseModule } from './database/database.module';
 @Module({
   imports: [
     CoffeesModule,
@@ -16,6 +18,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: false, // use migrations instead
     }),
+    CoffeeRatingModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
